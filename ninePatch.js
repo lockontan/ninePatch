1a7c3370f8ff384fe67f2795ac6afa95b5b9c854
lockontan <276104371@qq.com>
2023-06-24 12:53:45 +0800

    parse

--- ninePatch.js
+++ ninePatch.js
@@ -9,8 +9,8 @@ var ninePatch = (function () {
         readASCII: function (buff, p, l) {
             var s = "";
             for (var i = 0; i < l; i++) {
-                s += String.fromCharCode(buff[p + i])
-            };
+                s += String.fromCharCode(buff[p + i]);
+            }
             return s;
         }
     };
@@ -19,10 +19,10 @@ var ninePatch = (function () {
             offset = 8;
         var out = {};
 
-        var mgck = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
-        for (var i = 0; i < 8; i++)
-            if (data[i] != mgck[i]) throw "The input is not a PNG file!";
-
+        var pngFlag = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
+        for (var i = 0; i < 8; i++) {
+            if (data[i] != pngFlag[i]) throw "This is not a PNG file!";
+        }
         while (offset < data.length) {
             var len = bin.readUint(data, offset);
             offset += 4;
@@ -30,10 +30,39 @@ var ninePatch = (function () {
             offset += 4;
 
             if (type == "npTc") {
-                out.wasDeserialized = data[offset + 1];
-                out.numXDivs = data[offset + 2];
-                out.numYDivs = data[offset + 3];
-                out.numColors = data[offset + 4];
+                var index = offset + 1;
+                var keys = [
+                    { name: "wasDeserialized", byteLength: 1 },
+                    { name: "numXDivs", byteLength: 1 },
+                    { name: "numYDivs", byteLength: 1 },
+                    { name: "numColors", byteLength: 1 },
+                    { name: "xDivsOffset", byteLength: 4 },
+                    { name: "yDivsOffset", byteLength: 4 },
+                    { name: "paddingLeft", byteLength: 4 },
+                    { name: "paddingRight", byteLength: 4 },
+                    { name: "paddingTop", byteLength: 4 },
+                    { name: "paddingBottom", byteLength: 4 },
+                    { name: "colorsOffset", byteLength: 4 },
+                ];
+                for (var index = 0; index < keys.length; index++) {
+                    var key = keys[index];
+                    if (byteLength == 1) {
+                        out[key.name] = data[index];
+                    } else if (byteLength == 4) {
+                        out[key.name] = bin.readUint(data, index);
+                    }
+                    index += key.byteLength;
+                }
+                out.xDivs = [];
+                for (var index = 0; index < out.numXDivs; index++) {
+                    out.xDivs.push(bin.readUint(data, index));
+                    index += 4;
+                }
+                out.yDivs = [];
+                for (var index = 0; index < out.numYDivs; index++) {
+                    out.yDivs.push(bin.readUint(data, index));
+                    index += 4;
+                }
                 break;
             } else if (type == "IEND") {
                 break;
@@ -46,5 +75,5 @@ var ninePatch = (function () {
     }
     return {
         decode: decode
-    }
+    };
 })();
